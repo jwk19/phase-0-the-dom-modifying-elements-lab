877dbba1460efa74fddc8f331964b4ddf4eb290b
James <spesalink@gmail.com>
2024-07-15 08:37:40 +0300

    Created H! element using DOM element. Removed element with id main using remove DOM method.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+//create H1 element with variable newHeader
+const newheader = document.createElement(h1);
+//append the h1 to the body
+document.body.append(newheader);
+newheader.id = 'victory';
+
+//remove element with id='main'
+
+document.addEventListener("DOMContentLoaded", function() {
+    const mainElement = document.getElementById("main");
+    if (mainElement) {
+      mainElement.remove();
+    }
+  });
+  
